e216edd7362582404a6a44f9808c2c9391d89510
Mahad Sheikh <sheikhmahad17@gmail.com>
2025-08-22 21:31:09 -0400

    encrypting and decrypting

--- PassMan.py
+++ PassMan.py
@@ -1,23 +1,26 @@
 from cryptography.fernet import Fernet  # using this to encrypt the master password
 
 
-pwd = input("what is the master password? : ") # needed to encrypt the password that we save in the file
 
+# functions to encrypt and decrypt passwords for us
+# def write_key(): # this specific functions will be used to generate and save a key that we will later use to encrypt and decrypt (one time use only)
+#     key = Fernet.generate_key()
+#     with open('key.key','wb') as key_file:
+#         key_file.write(key)
 
+# write_key()     this was only used once to make the file that was for the key
 def load_key(): # used to read the key once written
     file = open('key.key','rb')
     key = file.read()
     file.close()
     return key
 
-# key = load_key()
-# functions to encrypt and decrypt passwords for us
-def write_key(): # this specific functions will be used to generate and save a key that we will later use to encrypt and decrypt (one time use only)
-    key = Fernet.generate_key()
-    with open('key.key','wb') as key_file:
-        key_file.write(key)
 
-# write_key()     this was only used once to make the file that was for the key
+master_pwd = input("what is the master password? : ") # needed to encrypt the password that we save in the file
+
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
 
 
 def view():
@@ -26,7 +29,7 @@ def view():
             # print(line.rstrip()) # strips the extra line that is being used to save the password
             data = line.rstrip() #this return a list of string so we split it and use the elements in it
             usr,passw = data.split("|")
-            print("Account Name : ",usr,", Password : ",passw)
+            print("Account Name : ",usr,", Password : ",fer.decrypt(passw.encode()).decode())
 
 
 def add():
@@ -37,7 +40,7 @@ def add():
     # file.close()
 
     with open('password.txt','a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
